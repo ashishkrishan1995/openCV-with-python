5ee082385caa23bae1c79ac80837c4b5c293e8d0
Guruji-D-Gr8 <pankaj.yadav.ece17@itbhu.ac.in>
2018-10-18 09:34:12 +0530

    python file for real time object detection using openCV

--- real_time_object_detection.py
+++ real_time_object_detection.py
@@ -22,6 +22,8 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 	"sofa", "train", "tvmonitor"]
+
+# array of different colors
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 # load our serialized model from disk
