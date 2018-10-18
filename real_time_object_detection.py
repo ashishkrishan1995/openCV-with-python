f9b53d3d99623b43199a67596e27c7ff7f96a3cb
Guruji-D-Gr8 <pankaj.yadav.ece17@itbhu.ac.in>
2018-10-18 09:29:50 +0530

    added real_time_object_detection.py

---
+++ real_time_object_detection.py
@@ -0,0 +1,101 @@
+'''
+This python file detects objects in real-time
+To run this file clone the CAFFE model used in this file from https://github.com/y-pankaj/Caffe-files-for-object-detection
+The repo contains two files. One of them is the pretrained CAFFE model which contains the weights and the other files is to deploy this model in out python file
+Put both the files in the same directory as this python file and then run this python file ( python real_time_object_detection.py for ubuntu)
+This file makes use of dnn module of OpenCV for object detection.
+The below code has comments along with it to make this file understandable.
+'''
+
+# importing the packages for the project
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+
+# these classes can be detected by this model
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe('MobileNetSSD_deploy.prototxt.txt', 'MobileNetSSD_deploy.caffemodel')
+
+# initializing the video stream
+# and initializng the FPS counter
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+# loop over the frames from the video stream
+while True:
+	# each frame is resized to a width of 600 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=600)
+
+	# grab the frame dimensions and convert it to a blob
+    # blobFromImage does mean substraction and scaling
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+		0.007843, (300, 300), 127.5)
+
+	# pass the blob through the network and obtain the detections and
+	# predictions
+	net.setInput(blob)
+	detections = net.forward()
+
+	for i in np.arange(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the prediction
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+		if confidence > .2:
+			# extract the index of the class label from the
+			# `detections`, then compute the (x, y)-coordinates of
+			# the bounding box for the object
+			idx = int(detections[0, 0, i, 1])
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+
+			width_in_pixels = endX - startX
+
+			# draw the prediction on the frame
+			label = "{}: {:.2f} %".format(CLASSES[idx],
+				confidence * 100)
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				COLORS[idx], 2)
+			y = startY - 15 if startY - 15 > 15 else startY + 15
+			cv2.putText(frame, label, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+			
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+cv2.destroyAllWindows()
+vs.stop()
